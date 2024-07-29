15909d16b949fe7b58aaab9a0393b7c8f55c6292
XProtocol0 <tyagiaviral@gmail.com>
2024-07-29 16:49:19 +0530

    initial commit

---
+++ src/test/java/com/github/foodie/FoodieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.foodie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
