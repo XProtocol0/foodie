c76975fd9ab1f9123c3bc0cdd94c69900c5c40c2
XProtocol0 <tyagiaviral@gmail.com>
2024-08-02 23:13:18 +0530

    adds enumValue validator

---
+++ src/main/java/validation/EnumValue.java
@@ -0,0 +1,20 @@
+package validation;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import validation.validator.EnumValueValidator;
+
+import java.lang.annotation.*;
+
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EnumValueValidator.class)
+public @interface EnumValue {
+    Class<? extends Enum<?>> enumClass();
+    String message() default "must be any of enum {enumClass}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
+
