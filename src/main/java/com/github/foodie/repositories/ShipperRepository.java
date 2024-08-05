6b48d2117989da42e9e478b52095d6227009e813
XProtocol0 <tyagiaviral@gmail.com>
2024-08-05 22:22:40 +0530

    adds findTenNearbyTopRatedShipper in ShipperRepository

--- src/main/java/com/github/foodie/repositories/ShipperRepository.java
+++ src/main/java/com/github/foodie/repositories/ShipperRepository.java
@@ -14,8 +14,15 @@ import java.util.UUID;
 public interface ShipperRepository extends JpaRepository<ShipperEntity, UUID> {
     @Query(value = "SELECT s.*, ST_Distance(s.location, :location) AS distance " +
             "FROM shipper s " +
-            "WHERE s.available = true AND ST_DWithin(s.location, :location, 10000) " +
-            "ORDER BY distance " +
+            "WHERE s.available = true AND ST_DWithin(s.location, :location, 1000) " +
+            "ORDER BY d.distance DESC " +
             "LIMIT 10", nativeQuery = true)
     List<ShipperEntity> findNearest10Shippers(Point location);
+
+    @Query(value = "SELECT s.*, ST_Distance(s.location :location) AS distance " +
+            "FROM shipper s " +
+            "WHERE s.available = true AND ST_DWithin(s.location, :location, 2000) " +
+            "ORDER BY d.rating " +
+            "LIMIT 10", nativeQuery = true)
+    List<ShipperEntity> findTenNearbyTopRatedShipper(Point location);
 }
