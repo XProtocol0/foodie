39bf82b5685802dd2b756c0c55ef2de36380faa2
XProtocol0 <tyagiaviral@gmail.com>
2024-08-16 22:15:23 +0530

    fixes JsonConverter

--- src/main/java/com/github/foodie/converters/JsonConverter.java
+++ src/main/java/com/github/foodie/converters/JsonConverter.java
@@ -5,14 +5,17 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
+import lombok.extern.slf4j.Slf4j;
 
 @Converter
+@Slf4j
 public class JsonConverter<T> implements AttributeConverter<T, String> {
     private final TypeReference<T> typeReference = new TypeReference<T>() {};
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public String convertToDatabaseColumn(T object) {
+        log.info("converting to database column");
         try {
             return objectMapper.writeValueAsString(object);
         } catch (JsonProcessingException e) {
@@ -22,6 +25,7 @@ public class JsonConverter<T> implements AttributeConverter<T, String> {
 
     @Override
     public T convertToEntityAttribute(String json) {
+        log.info("converting to entity attribute");
         try {
             return objectMapper.readValue(json, typeReference);
         } catch (JsonProcessingException e) {
